252ab22c0f42087a1cae1edd79b046b457a825eb
d3dd3d <94109862+d3dd3d@users.noreply.github.com>
2022-03-29 16:31:11 +0300

    Подключил библиотеки

--- main.js
+++ main.js
@@ -1,1 +1,61 @@
+<script type="text/javascript" src="../../jqwidgets/jqxcore.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxvalidator.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxinput.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxdatetimeinput.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxcalendar.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxtooltip.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxbuttons.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxscrollbar.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxlistbox.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxdropdownlist.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxdropdownbutton.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxcolorpicker.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxwindow.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxeditor.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxtooltip.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxcombobox.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxdata.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxlistbox.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxcombobox.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxcheckbox.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxnumberinput.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxmenu.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxgrid.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxgrid.pager.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxgrid.selection.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxdata.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxgrid.grouping.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxgrid.sort.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxgrid.filter.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxgrid.edit.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxgrid.columnsresize.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxgrid.columnsreorder.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxpanel.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxTabs.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxfileupload.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/jqxdatatable.js"></script>
+    <script type="text/javascript" src="~/jqwidgets/jqxgrid.storage.js"></script>
 
+    <script type="text/javascript" src="../../jqwidgets/jqxtextarea.js"></script>
+
+    <script type="text/javascript" src="../../jqwidgets/jqxgrid.aggregates.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/numberinput.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/globalization/globalize.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/globalization/localization.js"></script>
+    <script type="text/javascript" src="../../jqwidgets/globalization/globalize.culture.ru-RU.js"></script>
+    <script src="~/Scripts/leaflet/leaflet.js"></script>
+    <script src="~/Scripts/leaflet/leaflet-search.js"></script>
+    <script src="~/Scripts/leaflet/proj4.js"></script>
+    <script src="~/Scripts/leaflet/proj4leaflet.js"></script>
+    <script src="~/Scripts/leaflet/oms.min.js"></script>
+    <script src="~/Scripts/leaflet/leaflet_rotated_marker.js"></script>
+    <script src="~/Scripts/leaflet/easy-button.js"></script>
+    <script src="~/Scripts/leaflet/leaflet.textpath.js"></script>
+    <script src="~/Scripts/leaflet/wicket.js"></script>
+    <script src="~/Scripts/leaflet/wicket-leaflet.js"></script>
+    <script src="~/Scripts/leaflet/turf.min.js"></script>
+
+    <script type="text/javascript" src="/Scripts/moment.min.js"></script>
+    <script type="text/javascript" src="/Scripts/moment-duration-format.js"></script>
+    <script type="text/javascript" src="/Scripts/Pages/Order.js"></script>
+    <script type="text/javascript" src="/Scripts/geocoding.js"></script>

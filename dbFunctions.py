3bbe60982553bbc6cb1eb1e05843fc41701ea053
Nikolay <egorov_nikolay_n@mail.ru>
2022-04-30 14:35:53 +0400

    Added new functions

--- dbFunctions.py
+++ dbFunctions.py
@@ -3,7 +3,7 @@ from bson import regex as re
 import gridfs
 import os
 
-def searchQuery(database, queryText="", lastId=0):
+def searchQuery(database, queryText=""):
     """Returns cursor with 30 tracks whose names contain queryText.
 
     Keyword arguments:
@@ -12,10 +12,7 @@ def searchQuery(database, queryText="", lastId=0):
     lastId -- last _id value from previous query. This and previous ids won't be
               in cursor. Leave it empty if you don't need to skip documents.
     """
-    if not isinstance(lastId, int) or lastId < 0:
-        raise ValueError('lastId must be greater then or equal to 0')
-    else:
-        return database.tracks.find({"_id":{"$gt":lastId},"track_name":re.Regex(".*" + str(queryText) + ".*", 'i')}).limit(30)
+    return database.tracks.find({"track_name":re.Regex(".*" + str(queryText) + ".*", 'i')})
 
 def getNewTrackId(database):
     """Returns maximum of _id field + 1 to set id for new track
@@ -51,6 +48,9 @@ def uploadTrack(client, database, track_name, track_description=None, track_phot
                     raise ValueError("track_description must be string value")
             new_id = database.tracks.insert_one(new_document).inserted_id
             if track_photo_path is not None:
+                extension = track_photo_path.split(".")[-1]
+                if(extension != "jpg" and extension != "jpeg" and extension != "png" and extension != "bmp"):
+                    raise ValueError("file extension must be jpg or jpeg or png or bmp")
                 if isinstance(track_photo_path, str):
                     try:
                         file_data = open(track_photo_path, "rb")
@@ -63,6 +63,9 @@ def uploadTrack(client, database, track_name, track_description=None, track_phot
                 else:
                     raise ValueError("track_photo_path must be string value")
             if track_file_path is not None:
+                extension = track_file_path.split(".")[-1]
+                if(extension != "mp3" and extension != "wav" and extension != "ogg"):
+                    raise ValueError("file extension must be mp3 or wav or ogg")
                 if isinstance(track_file_path, str):
                     try:
                         file_data = open(track_file_path, "rb")
@@ -75,7 +78,7 @@ def uploadTrack(client, database, track_name, track_description=None, track_phot
                 else:
                     raise ValueError("track_file_path must be string value")
 
-def downloadTrackImage(database, track_id, download_path):
+def downloadTrackImage(database, track_id, download_path, file_name=None):
     """Downloads image of track. Returns true if success.
     
     Keyword arguments:
@@ -87,18 +90,14 @@ def downloadTrackImage(database, track_id, download_path):
         raise ValueError("track_id must be Number value")
     if not isinstance(download_path, str):
         raise ValueError("download_path must be string value")
-    fullpath = ""
-    if str(download_path).find("/") == -1:
-        fullpath = str(download_path).replace("\\", "/")
-    else:
-        fullpath = download_path
+    fullpath = download_path.replace("\\", "/") if download_path.find("/") == -1 else download_path
     if fullpath[-1] != "/":
         fullpath += "/"
     fs = gridfs.GridFS(database)
     data = database.fs.files.find_one({"filename":re.Regex(str(track_id)+"_img.*")})
     if data is None:
         return False
-    fullpath += data["filename"]
+    fullpath += data["filename"] if file_name is None else file_name 
     if os.path.exists(fullpath):
         raise ValueError("File with such name already exists")
     fs_id = data["_id"]
@@ -107,3 +106,47 @@ def downloadTrackImage(database, track_id, download_path):
     output.write(outputdata)
     output.close()
     return True
+
+def downloadTrackAudio(database, track_id, download_path, file_name = None):
+    """Downloads audiofile. Returns true if success.
+    
+    Keyword arguments:
+    database -- instance of database
+    track_id -- id of track in db
+    download_path -- full path for directory to download audio
+    """
+    if not isinstance(track_id, int) and not isinstance(track_id, float):
+        raise ValueError("track_id must be Number value")
+    if not isinstance(download_path, str):
+        raise ValueError("download_path must be string value")
+    fullpath = download_path.replace("\\", "/") if download_path.find("/") == -1 else download_path
+    if fullpath[-1] != "/":
+        fullpath += "/"
+    fs = gridfs.GridFS(database)
+    data = database.fs.files.find_one({"filename":re.Regex(str(float(track_id))+"_audio.*")})
+    if data is None:
+        return False
+    fullpath += data["filename"] if file_name is None else file_name 
+    if os.path.exists(fullpath):
+        raise ValueError("File with such name already exists")
+    fs_id = data["_id"]
+    outputdata = fs.get(fs_id).read()
+    output = open(fullpath, "wb")
+    output.write(outputdata)
+    output.close()
+    return True
+
+def deleteTrack(client, database, track_id):
+    """Delete track from database
+    
+    Keyword arguments:
+    client -- instance of MongoClient
+    database -- instance of database
+    track_id -- id of track in db
+    """
+    result = 0
+    with client.start_session() as session:
+        with session.start_transaction():
+            result = database.tracks.delete_one({"_id":track_id}).acknowleged
+            database.fs.files.delete_many({"filename":re.Regex(str(track_id) + ".*", 'i')})
+    return result
---
+++ test.py
@@ -0,0 +1,36 @@
+#Здесь можно протестировать функции
+
+from dbFunctions import *
+import tkinter
+
+def CheckSearch(database, query = ""):
+    """Проверка вывода треков"""
+    for cur in searchQuery(database, query):
+        s=""
+        for x in cur:
+            s+=str(cur[x])+" "
+        print(s)
+
+client = MongoClient()
+database = client["dbmashup"]
+
+CheckSearch(database)
+
+# deleteTrack(client, database, 4)
+# deleteTrack(client, database, 5)
+
+## пример загрузки
+# try:
+#     uploadTrack(client, database, "done - Rap God Rick", None, "someimage.jpg", "audio/done - Rap God Rick.mp3")
+# except Exception() as exc:
+#     print(exc)
+# try:
+#     uploadTrack(client, database, "heyoka vs cage - the elephant", "grimeatron mash up", None, "audio/heyoka vs cage - the elephant (grimeatron mash up).mp3")
+# except Exception() as exc:
+#     print(exc)
+
+## Скачивание по id 
+#print(downloadTrackImage(database, 4, "путь к папке для скачивания"))
+# print(downloadTrackAudio(database, 4, "путь к папке для скачивания"))
+
+client.close()

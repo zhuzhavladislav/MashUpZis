8c9d3a5066ae68fa2e9569128c90dd58da65caf8
Nikolay <egorov_nikolay_n@mail.ru>
2022-05-22 15:47:24 +0400

    Simplified site structure

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
-const http = require('http');
-const routing = require('./routing');
+const express = require('express');
 
-let server = new http.Server(function(req, res) {
-  var jsonString = '';
-  res.setHeader('Content-Type', 'application/json');
-  req.on('data', (data) => { 
-      jsonString += data;
-  });
+let app = express();
 
-  req.on('end', () => {
-      routing.define(req, res, jsonString); 
-  });
+app.use(express.static(__dirname + '/public'));
+app.use(function(request, response, next){
+  console.log(request.url);
+  next();
 });
-server.listen(3000, 'localhost', function(){
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/public/index.html'); 
+});
+app.get('/search', function(request, response){
+  response.sendFile(__dirname + '/public/search.html');
+})
+
+app.listen(3000, function(){
   console.log('Сервер запущен на порту 3000');
 });
